ab451b356ad36a9ed251593ec65f527fc7c05103
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-04-13 21:50:34 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
       <Head>
         <title></title>
         <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="" />
       </Head>
     </div>
